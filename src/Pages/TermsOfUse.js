682ccdd481ab78a0051cd7dc71aad58a2f1a9241
Eric Brownson <brownsrikky@gmail.com>
2023-12-14 12:38:56 +0100

    Commit

--- src/Pages/TermsOfUse.js
+++ src/Pages/TermsOfUse.js
@@ -2,13 +2,85 @@ import React from 'react'
 
 function TermsOfUse() {
   return (
-    <div>
+    <div className='terms-of-use'>
         <h1>Terms and Conditions</h1>
         <p>
             Welcome to DBI MEDIA HUB, your one-stop shop for streaming your favorite music!
-             By accessing and using our platform, you agree to be bound by these Terms of Use ("Terms").
-              Please read these Terms carefully before using DBI MEDIA HUB.
-              </p>
+            By accessing and using our platform, you agree to be bound by these Terms of Use ("Terms").
+            Please read these Terms carefully before using DBI MEDIA HUB.
+        </p>
+
+        <p><b>1. Content and Services</b></p>
+
+        <p>DBI MEDIA HUB provides a platform for users to access and stream music licensed from third-party music providers.
+           We do not own or control the music content available on the platform. 
+           Your use of the music content is subject to the terms and conditions of the respective music providers.
+        </p>
+
+        <p><b>2. Acceptable Use</b></p>
+
+        <p>You may only use DBI MEDIA HUB for lawful purposes and in accordance with these Terms. You agree not to:</p>
+
+        <ul>
+            <li>Access or attempt to access DBI MEDIA HUB in any way that is not authorized by these Terms.</li>
+            <li>Use DBI MEDIA HUB for any illegal or unauthorized purpose, including, but not limited to, copyright infringement, hacking, or spamming.</li>
+            <li>Share your account information with anyone else</li>
+            <li>Modify, reverse engineer, or decompile any part of DBI MEDIA HUB.</li>
+            <li>Do anything that could harm the reputation of DBI MEDIA HUB or its users.</li>
+        </ul>
+
+        <p><b>3. User Content</b></p>
+
+        <p>
+            You are solely responsible for any content you submit to DBI MEDIA HUB, including, but not limited to, comments, reviews, and playlists. You agree to:
+        </p>
+
+        <ul>
+            <li>Not submit any content that is illegal, offensive, or infringes on the rights of others.</li>
+            <li>Not impersonate any person or entity.</li>
+            <li>Not submit any content that contains viruses or other harmful code.</li>
+        </ul>
+
+        <p>
+            DBI MEDIA HUB reserves the right to remove any user content that violates these Terms or is otherwise deemed inappropriate.
+        </p>
+
+        <p><b>4. Disclaimer of Warranties</b></p>
+
+        <p>
+          DBI MEDIA HUB is provided on an "as is" and "as available" basis.
+          We make no warranties, express or implied, of any kind with respect to DBI MEDIA HUB or the content available on it.
+          We do not warrant that DBI MEDIA HUB will be uninterrupted, error-free, or secure.
+        </p>
+
+        <p><b>5. Limitation of Liability</b></p>
+
+        <p>
+          DBI MEDIA HUB will not be liable for any damages arising out of or related to your use of DBI MEDIA HUB, including, but not limited to, direct, indirect, incidental, consequential, and punitive damages.
+        </p>
+
+        <p><b>6. Termination</b></p>
+
+        <p>We may terminate your access to DBI MEDIA HUB for any reason at any time without notice.</p>
+
+        <p><b>7. Governing Law and Jurisdiction</b></p>
+
+        <p>
+            These Terms shall be governed by and construed in accordance with the laws of NIGERIA,
+            without regard to its conflict of laws provisions.
+        </p>
+
+        <p><b>8. Changes to Terms of Use</b></p>
+
+        <p>We reserve the right to modify these Terms at any time. Your continued use of DBI MEDIA HUB after any changes are made will be considered your acceptance of the new Terms.</p>
+
+        <p><b>9. Contact Us</b></p>
+
+        <p>If you have any questions about these Terms, please contact us at <a>digitalblazeinnovations@gmail.com</a>.</p>
+
+        <h3><b>Thank you for using DBI MEDIA HUB!</b></h3>
+        
+
     </div>
   )
 }
